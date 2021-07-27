6e67fc89459f0157b3516a56002d6677449aa098
Gg123 <li.c@mail.ru>
2021-07-28 01:06:47 +0300

    Merge remote-tracking branch 'origin/master'

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,15 +1,42 @@
 package ru.job4j.oop;
 
 public class Cat {
-    public String sound() {
-        String voice = "may-may";
-        return voice;
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.println(this.food);
     }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void full() {
+        System.out.println(this.name);
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+
+    }
+
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
-        String say = peppy.sound();
-        System.out.println("Peppy says " + say);
+        System.out.println("There are gav's name.");
+        Cat gav = new Cat();
+        gav.giveNick("Гав");
+        gav.full();
+        System.out.println("There are gav's food.");
+        gav.eat("kotleta");
+        gav.show();
+
+        System.out.println("There are black's name.");
+        Cat black = new Cat();
+        black.giveNick("Чёрный");
+        black.full();
+        System.out.println("There are black's food.");
+        black.eat("fish");
+        black.show();
 
     }
 }
