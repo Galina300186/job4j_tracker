bed29f3020a2d28118a7a64b95c573551c921bc8
Gg123 <li.c@mail.ru>
2021-07-30 01:16:05 +0300

    Merge remote-tracking branch 'origin/master'

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -5,15 +5,13 @@ public class Cat {
     private String name;
 
     public void show() {
+        System.out.println(this.name);
         System.out.println(this.food);
+
     }
 
     public void eat(String meat) {
         this.food = meat;
-    }
-
-    public void full() {
-        System.out.println(this.name);
     }
 
     public void giveNick(String nick) {
@@ -22,18 +20,18 @@ public class Cat {
     }
 
     public static void main(String[] args) {
+
         System.out.println("There are gav's name.");
         Cat gav = new Cat();
         gav.giveNick("Гав");
-        gav.full();
         System.out.println("There are gav's food.");
         gav.eat("kotleta");
         gav.show();
 
+
         System.out.println("There are black's name.");
         Cat black = new Cat();
         black.giveNick("Чёрный");
-        black.full();
         System.out.println("There are black's food.");
         black.eat("fish");
         black.show();
