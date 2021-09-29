931f4c73d7b794683c668a630b75a7f80cace18f
Gg123 <li.c@mail.ru>
2021-09-29 20:32:01 +0300

    Merge remote-tracking branch 'origin/master'

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,35 @@ public class Item {
     private int id;
     private String name;
 
+    public Item() {
+    }
+
+    public Item(int id) {
+        this.id = id;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void printInfo() {
+        System.out.println();
+        System.out.println("id: " + id);
+        System.out.println("name: " + name);
+
+    }
+
+    public static void main(String[] args) {
+        Item first = new Item();
+        first.printInfo();
+        Item second = new Item(456789);
+        second.printInfo();
+        Item third = new Item(456789, "Java");
+        third.printInfo();
+
+    }
+
     public int getId() {
         return id;
     }
