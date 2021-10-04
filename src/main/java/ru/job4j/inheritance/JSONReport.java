19bcceb41bea2a7a7cab3e26c1d06fa7ce67ddf6
Gg123 <li.c@mail.ru>
2021-10-04 16:26:35 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,16 @@
+package ru.job4j.inheritance;
+
+public class JSONReport extends TextReport {
+
+    public static void main(String[] args) {
+        TextReport report = new TextReport();
+        String text = report.generate("Report's name", "Report's body");
+        System.out.println(text);
+    }
+
+    public String generate(String name, String body) {
+        return name + ":name"  + "," + body + ":body";
+    }
+
+}
+
