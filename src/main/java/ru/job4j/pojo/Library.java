e7f6ec8ea2f507212db40aec3a2453da5b136449
Gg123 <li.c@mail.ru>
2021-10-13 12:52:02 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,28 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book fantastic = new Book("Confrontation", 43);
+        Book science = new Book("Sapiens", 30);
+        Book child = new Book("Vinni Puh", 80);
+        Book educational = new Book("Clean code", 62);
+        Book[] volume = new Book[4];
+        volume[0] = fantastic;
+        volume[1] = science;
+        volume[2] = child;
+        volume[3] = educational;
+        for (int index = 0; index < volume.length; index++) {
+            Book vol = volume[index];
+            System.out.println(vol.getName() + " " + vol.getCount());
+        }
+        System.out.println("Replace fantastic to educational");
+        volume[0] = educational;
+        volume[3] = fantastic;
+        for (int index = 0; index < volume.length; index++) {
+            Book vol = volume[index];
+            if (vol.getName() == "Clean code") {
+                System.out.println(vol.getName() + " " + vol.getCount());
+            }
+        }
+    }
+}
