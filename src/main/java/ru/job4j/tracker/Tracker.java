54bb072019df808f706db3263369076fd1e6423b
Gg123 <li.c@mail.ru>
2021-11-05 00:07:18 +0300

    Merge remote-tracking branch 'origin/master'

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,8 @@
 package ru.job4j.tracker;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +12,25 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        String itemsStr = "";
+        Item[] itemsWithoutNull = new Item[this.items.length];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            Item item = items[i];
+            if (items[i] != null) {
+                System.out.println(items[i].getName());
+                itemsWithoutNull[count] = this.items[i];
+                count++;
+            }
+        }
+        return Arrays.copyOf(itemsWithoutNull, count);
+    }
+
+    public Item[] findByName(String key) {
+        return Arrays.copyOf(items, size);
     }
 
     public Item findById(int id) {
