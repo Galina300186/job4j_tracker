fd23746acb76ba4df4a76cfa1f3142bdb318f921
Gg123 <li.c@mail.ru>
2021-12-17 14:48:11 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public String travel() {
+        return null;
+    }
+
+    @Override
+    public int passengers() {
+        return 0;
+    }
+
+    @Override
+    public String fill(String price) {
+        return null;
+    }
+}
---
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,7 @@
+package ru.job4j.poly;
+
+public interface Transport {
+String travel();
+int passengers();
+String fill(String price);
+}
