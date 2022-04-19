46d1f274b1f46ad9bb3e4be34639d708e430459d
Gg123 <li.c@mail.ru>
2022-04-19 17:06:08 +0300

    0.3. Тестирование исключений с junit

--- src/test/java/ru/job4j/ex/FactTest.java
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -5,6 +5,6 @@ import org.junit.Test;
 public class FactTest {
     @Test(expected = IllegalArgumentException.class)
     public void whenLessThen0() {
-        int rsl = Fact.calc (-1);
+        int rsl = Fact.calc (- 1);
     }
     }
