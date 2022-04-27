b4b2a5595c642182d67514994e1aa967ca269a8f
Gg123 <li.c@mail.ru>
2022-04-27 16:25:34 +0300

    9.2. Тесты вывода на консоль

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -71,25 +71,36 @@ public class StartUITest {
     public void whenFindByItem() {
         Output out = new StubOutput();
         Tracker tracker = new Tracker();
-        Item item = new Item("Test1");
-        tracker.add(item);
-        FindByItem act = new FindByItem(out);
-        act.execute(new StubInput(new String[]{item.getName()}), tracker);
+        Item item = tracker.add(new Item("Test"));
+        String name = "Test";
+        Input in = new StubInput(
+                new String[]{"0", name, "1"}
+        );
+        UserAction[] actions = new UserAction[]{
+                new FindByItem(out),
+                new Exit(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
         String ln = System.lineSeparator();
-        String expect = new StringJoiner(ln, " ", ln)
-                .add(item.getName())
-                .toString();
         assertThat(out.toString(), is(
-                "=== Find items by name ===" + ln + item + ln));
+                "Menu." + ln
+                + "0. Find Item by name" + ln
+                + "1. Exit Program" + ln
+                + "=== Find items by name ===" + ln + item + ln
+                + "Menu." + ln
+                + "0. Find Item by name" + ln
+                + "1. Exit Program" + ln
+        ));
     }
 
     @Test
     public void whenFindByID() {
         Output out = new StubOutput();
-        Input in = new StubInput(
-        new String[]{"1", "1"}
-        );
         Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("testID"));
+        Input in = new StubInput(
+                new String[]{"0", String.valueOf(item.getId()), "1"}
+        );
         UserAction[] actions = {
                 new FindItemByID(out),
                 new Exit(out)
@@ -98,6 +109,11 @@ public class StartUITest {
         String ln = System.lineSeparator();
         assertThat(out.toString(), is(
                 "Menu." + ln
+                        + "0. Find Item by Id" + ln
+                        + "1. Exit Program" + ln
+                        + "=== Find item by id ===" + ln
+                        + item + ln
+                        + "Menu." + ln
                         + "0. Find Item by Id" + ln
                         + "1. Exit Program" + ln
         ));
